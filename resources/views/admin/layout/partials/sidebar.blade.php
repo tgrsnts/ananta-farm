61b85aa183806946f45af5d9e4f31de9d10e6916
tgrsnts <tegarsantoso72@gmail.com>
2025-04-29 16:05:14 +0700

    fix profile

--- resources/views/admin/layout/partials/sidebar.blade.php
+++ resources/views/admin/layout/partials/sidebar.blade.php
@@ -75,7 +75,7 @@
             class="{{ $currentRoute == 'admin/staycation' ? 'text-green-normal bg-white' : 'text-white' }} font-poppins font-semibold flex items-center w-full py-4 pl-16 pr-8 leading-tight transition-all rounded-r-lg outline-none text-start hover:bg-white hover:text-green-normal focus:bg-white focus:text-green-normal active:bg-white active:text-green-normal">
             <div class="flex w-12 h-12 mr-4 items-center justify-center">
                 <img class="w-full aspect-square rounded-full"
-                src="{{ $user->foto ? asset('storage/' . $user->foto) : asset('assets/image/avatar-biru.jpg') }}"
+                src="{{ Auth()->user()->foto ? asset('storage/' . Auth()->user()->foto) : asset('assets/image/avatar-biru.jpg') }}"
                 alt="Foto Profil" />
             </div>
            <flex class="flex-col">
