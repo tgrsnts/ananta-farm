4020abd4e755cbf993a29bc5f49192eb3416ffb8
Takulian <miftakhulghulam10@gmail.com>
2025-04-30 18:40:35 +0700

    katalog update works now

--- app/Http/Controllers/KatalogController.php
+++ app/Http/Controllers/KatalogController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Katalog;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 class KatalogController extends Controller
 {
@@ -42,6 +43,29 @@ class KatalogController extends Controller
             ]);
         }
         return redirect('/admin/katalog');
+    }
+
+    public function update(Request $request, $id){
+        $katalog = Katalog::findOrFail($id);
+        $katalog->update([
+           'nama' => $request->nama,
+            'bobot' => $request->bobot,
+            'harga' => $request->harga,
+            'jenis' => $request->jenis,
+        ]);
+        if ($request->hasFile('foto')) {
+            $pathLama = storage_path('app/public/' . $katalog->foto);
+            if (File::exists($pathLama)) {
+                File::delete($pathLama);
+            }
+            $file = $request->file('foto');
+            $fileName = $request->bobot. $katalog->id . '.' . $file->extension();
+            $path = $file->storeAs('fotoKatalog', $fileName, 'public');
+            $katalog->update([
+                'foto' => $path
+            ]);
+        }
+        return redirect()->route('admin.katalog.index');
     }
 
     public function destroy($id){
